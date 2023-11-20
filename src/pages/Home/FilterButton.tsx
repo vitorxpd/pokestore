6607938621bd33e317af79279bcdeaedfa2e1450
Vitor Diniz <dinizadroit@gmail.com>
2023-11-20 17:40:39 -0300

    feat: home

---
+++ src/pages/Home/FilterButton.tsx
@@ -0,0 +1,26 @@
+import filterIcon from '../../assets/icons/filter.svg';
+import { cn } from '../../utils/cn';
+
+interface FilterProps {
+  className: string;
+}
+
+export function FilterButton({ className }: FilterProps) {
+  return (
+    <button
+      id="but"
+      className={cn(
+        'w-[50px] desktop:w-[76px] h-[50px] desktop:h-[76px] flex items-center justify-center',
+        'border border-solid border-[#E0E0E0] rounded-full',
+        'shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]',
+        className,
+      )}
+    >
+      <img
+        src={filterIcon}
+        alt="Filter"
+        className="desktop:w-[50px] desktop:h-[50px]"
+      />
+    </button>
+  );
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useRef, useState } from 'react';
+
+import { cn } from '../../utils/cn';
+
+import { Card } from './Card';
+import { FilterButton } from './FilterButton';
+
+export function Home() {
+  const [filterIsFixed, setFilterIsFixed] = useState(false);
+
+  const containerRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    function scrollMonitor() {
+      if (!containerRef.current) {
+        return;
+      }
+
+      window.scrollY >= containerRef.current.offsetTop
+        ? setFilterIsFixed(true)
+        : setFilterIsFixed(false);
+    }
+
+    window.addEventListener('scroll', scrollMonitor);
+
+    return () => {
+      window.removeEventListener('scroll', scrollMonitor);
+    };
+  }, []);
+
+  return (
+    <main
+      ref={containerRef}
+      className="max-w-[1440px] mx-auto my-0 pt-[72px] desktop:pt-11 pb-[30px] desktop:pb-20 px-4 flex justify-center relative"
+    >
+      <FilterButton
+        className={cn(
+          'top-2 desktop:top-11 right-[14px] desktop:right-[-18px]',
+          filterIsFixed ? 'fixed desktop:absolute' : 'absolute',
+        )}
+      />
+
+      <ul className="grid grid-cols-[repeat(2,calc(180px+24px))] desktop:grid-cols-[repeat(4,calc(275px+37px))] gap-[14px] desktop:gap-[38px]">
+        <Card />
+      </ul>
+    </main>
+  );
+}
