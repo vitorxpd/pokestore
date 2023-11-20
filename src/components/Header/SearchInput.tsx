685d86ef74beea6f748881aa9d9650227f468b41
Vitor Diniz <dinizadroit@gmail.com>
2023-11-20 15:45:15 -0300

    fix: border style

--- src/components/Header/SearchInput.tsx
+++ src/components/Header/SearchInput.tsx
@@ -7,7 +7,7 @@ export function SearchInput() {
       className={cn(
         'w-full desktop:w-[416px] h-9 desktop:h-[42px] py-3 pl-4 desktop:pl-5 pr-[52px] desktop:pr-15',
         'text-[10px] desktop:text-xs placeholder:text-[10px] desktop:placeholder:text-xs placeholder:text-[#B0B0B0]',
-        'border border-[#E0E0E0] rounded-[30px] outline-none',
+        'border border-solid border-[#E0E0E0] rounded-[30px] outline-none',
       )}
     />
   );
