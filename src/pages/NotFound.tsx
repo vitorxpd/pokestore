06b0bad88966bbeb9d1dcb8a905dcfe91ee6f87b
Vitor Diniz <dinizadroit@gmail.com>
2023-11-20 15:06:33 -0300

    feat: 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,28 @@
+import { Link } from 'react-router-dom';
+
+import snorlax from '../assets/snorlax.svg';
+
 export function NotFound() {
-  return <h1>NotFound</h1>;
+  return (
+    <div className="mt-[54px] pb-4">
+      <img
+        src={snorlax}
+        alt="Snorlax"
+        className="mx-auto my-0 desktop:w-[400px] desktop:h-[374px]"
+      />
+
+      <div className="mt-6 desktop:mt-[26px] flex flex-col items-center gap-1">
+        <p className="text-xs desktop:text-base opacity-60">
+          error 404: page not found :(
+        </p>
+
+        <Link
+          to="/"
+          className="text-xs desktop:text-base underline opacity-40 block"
+        >
+          go to home
+        </Link>
+      </div>
+    </div>
+  );
 }
