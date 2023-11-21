2ca0e7cec9b1b568c30f4dd2a69a97b850f4233a
Vitor Diniz <dinizadroit@gmail.com>
2023-11-21 20:28:44 -0300

    chore: setting the font in the html

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -4,7 +4,7 @@ import { Header } from '../components/Header';
 
 export function DefaultLayout() {
   return (
-    <div className="font-primary">
+    <div>
       <Header />
       <Outlet />
     </div>
