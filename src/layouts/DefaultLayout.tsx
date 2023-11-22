c933c7f3fb6f5d37bdce4db0eb15be7d5a45299a
Vitor Diniz <dinizadroit@gmail.com>
2023-11-22 18:51:15 -0300

    feat: add layout classname

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -4,7 +4,7 @@ import { Header } from '../components/Header';
 
 export function DefaultLayout() {
   return (
-    <div>
+    <div className="layout">
       <Header />
       <Outlet />
     </div>
