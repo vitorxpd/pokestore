5b7f7fbdbec9fa3549bf04a357dee6ab0ee087c3
Vitor Diniz <dinizadroit@gmail.com>
2023-11-22 20:12:12 -0300

    feat: pokemon page

--- src/pages/Pokemon/index.tsx
+++ src/pages/Pokemon/index.tsx
@@ -1,11 +1,68 @@
-import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-interface PokemonParams {
-  name: string;
-}
+import dragonite from '../../assets/dragonite.svg';
+import questionIcon from '../../assets/icons/question.svg';
+import { PokemonCard } from '../../components/PokemonCard';
+import { PokemonModal } from '../../components/PokemonModal';
+import { Spinner } from '../../components/Spinner';
+
+import { usePokemonController } from './usePokemonController';
 
 export function Pokemon() {
-  const { name } = useParams<Partial<PokemonParams>>();
+  const {
+    currentPokemon,
+    modalIsOpen,
+    isLoading,
+    key,
+    handleOpenModal,
+    handleCloseModal,
+  } = usePokemonController();
 
-  return <main>{name}</main>;
+  return (
+    <main className="max-w-[1440px] mx-auto px-11 py-9">
+      {isLoading && (
+        <Spinner className="mx-auto desktop:w-12 desktop:h-12 mt-6" />
+      )}
+
+      {!isLoading && currentPokemon && (
+        <div className="flex justify-center">
+          <PokemonCard pokemon={currentPokemon} onOpenModal={handleOpenModal} />
+
+          <PokemonModal
+            pokemon={currentPokemon}
+            open={modalIsOpen}
+            onClose={handleCloseModal}
+          />
+        </div>
+      )}
+
+      {!isLoading && !currentPokemon && (
+        <div>
+          <p className="text-xs desktop:text-3xl opacity-80">
+            results for {key}
+          </p>
+
+          <div className="w-[174px] desktop:w-[286px] h-56 desktop:h-[348px] mt-[70px] desktop:mt-[102px] mx-auto relative">
+            <img src={dragonite} alt="Dragonite" className="w-full" />
+
+            <img
+              src={questionIcon}
+              alt="Question"
+              className="desktop:w-[72px] desktop:h-[76px] absolute left-11 bottom-16 desktop:left-[78px] desktop:bottom-[98px]"
+            />
+          </div>
+
+          <div className="mt-[38px] desktop:mt-8 flex flex-col items-center gap-3">
+            <p className="text-[10px] desktop:text-sm text-center opacity-60">
+              the pokédex doesn&apos;t have any info about this pokémon :(
+            </p>
+
+            <Link to="/" className="text-[10px] desktop:text-sm opacity-40">
+              return to the home
+            </Link>
+          </div>
+        </div>
+      )}
+    </main>
+  );
 }
---
+++ src/pages/Pokemon/usePokemonController.ts
@@ -0,0 +1,94 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { pokemonsService } from '../../services/pokemonsService';
+import { Pokemon } from '../../types/pokemon';
+import { delay } from '../../utils/delay';
+import { getPokemonPrice } from '../../utils/getPokemonPrice';
+import { pokemonColors } from '../../utils/pokemonColors';
+
+interface PokemonParams {
+  key: string;
+}
+
+export function usePokemonController() {
+  const [currentPokemon, setCurrentPokemon] = useState<null | Pokemon>(null);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { key } = useParams<Partial<PokemonParams>>();
+
+  function handleOpenModal() {
+    setModalIsOpen(true);
+  }
+
+  function handleCloseModal() {
+    setModalIsOpen(false);
+  }
+
+  useEffect(() => {
+    setCurrentPokemon(null);
+
+    const controller = new AbortController();
+
+    async function loadPokemon() {
+      setIsLoading(true);
+      await delay();
+
+      try {
+        const {
+          pokemon: {
+            base_experience,
+            height,
+            id,
+            name,
+            sprites,
+            types,
+            weight,
+          },
+        } = await pokemonsService.getPokemon(key ?? '', controller.signal);
+
+        const { price } = await getPokemonPrice(name);
+
+        const formattedTypes = types.map(({ type }) => {
+          return {
+            name: type.name,
+            color: pokemonColors[type.name],
+          };
+        });
+
+        const newPokemon: Pokemon = {
+          base_experience,
+          height,
+          id,
+          name,
+          price,
+          sprite: sprites.other.dream_world.front_default,
+          types: formattedTypes,
+          weight,
+        };
+
+        setCurrentPokemon(newPokemon);
+      } catch {
+        //
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    loadPokemon();
+
+    return () => {
+      controller.abort();
+    };
+  }, [key]);
+
+  return {
+    currentPokemon,
+    modalIsOpen,
+    isLoading,
+    key,
+    handleOpenModal,
+    handleCloseModal,
+  };
+}
